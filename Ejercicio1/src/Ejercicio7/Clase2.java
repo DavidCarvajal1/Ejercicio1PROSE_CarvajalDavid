4bdda403e4321d2ff8e87b7851913b016ac7d339
DavidCarvajal1 <david.carvajal@iesnervion.es>
2022-10-06 10:34:13 +0200

    aa

---
+++ Ejercicio1/src/Ejercicio7/Clase2.java
@@ -0,0 +1,7 @@
+package Ejercicio7;
+
+public class Clase2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Ejercicio1/src/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,14 @@
+package Ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        //Creamos una lista de ProcessBuilders
+        List<ProcessBuilder> lpb=new ArrayList<>();
+        //Añadimos los ProcessBuilders a la lista
+        lpb.add(new ProcessBuilder());
+        lpb.add(new ProcessBuilder());
+    }
+}
